63981456e0e6169330fbd4c69edbb56cd026f238
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-03-05 19:56:22 +0530

    Header logo redirect change

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 
     <header class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="#">Logo</a>
+    <a class="navbar-brand" href="index.php">Logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
