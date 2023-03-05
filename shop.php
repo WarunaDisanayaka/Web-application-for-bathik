6ed6518cb1ac1f5be29076dab38f71115c20fcbe
warunahello@gmail.com <waruna_2019515@fot.ruh.ac.lk>
2023-03-06 01:47:46 +0530

    Make shop page

---
+++ shop.php
@@ -0,0 +1,30 @@
+<?php
+
+// Include the header file
+require_once  'header.php';
+
+?>
+
+<!-- Hero section start-->
+
+<section class="hero" style="background-image: url('img/hero.png');">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-6">
+        <h1 class="hero-title">Buddhi batik</h1>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- Hero section end -->
+
+
+
+
+<?php
+
+// Include the footer file
+require_once 'footer.php';
+
+?>
