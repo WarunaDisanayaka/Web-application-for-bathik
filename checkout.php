00c0850fcc02a8be610f484aac1b40ae7e511119
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-04-29 23:23:28 +0530

    update

--- checkout.php
+++ checkout.php
@@ -1,116 +1,116 @@
 <?php
-   // Include the header file
-   require_once  'header.php';
-   
-   ?>
+// Include the header file
+require_once 'header.php';
+
+?>
 <?php
-   session_start();
+session_start();
 
-   if (!isset($_SESSION['email'])) {
-      // Redirect to the login page
-      header("Location: login.php");
-      exit();
-  }
+if (!isset($_SESSION['email'])) {
+   // Redirect to the login page
+   header("Location: login.php");
+   exit();
+}
 
-   //  Connect to the database
-   $dsn = 'mysql:host=localhost;dbname=bathik';
-   $username = 'root';
-   $password = '';
-   $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-   $pdo = new PDO($dsn, $username, $password, $options);
-   
-    // Select cart details
-    $stmt = $pdo->query("SELECT * FROM cart");  
-   
-     // To product details
-     $stmt2 = $pdo->query("SELECT * FROM cart");
-     
-     // Check if form was submitted
-   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
+//  Connect to the database
+$dsn = 'mysql:host=localhost;dbname=bathik';
+$username = 'root';
+$password = '';
+$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+$pdo = new PDO($dsn, $username, $password, $options);
+
+// Select cart details
+$stmt = $pdo->query("SELECT * FROM cart");
+
+// To product details
+$stmt2 = $pdo->query("SELECT * FROM cart");
+
+// Check if form was submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
    // Connect to MySQL database
    $servername = 'localhost';
    $username = 'root';
    $password = '';
    $dbname = 'bathik';
-   
+
    $conn = new mysqli($servername, $username, $password, $dbname);
-   
+
    if ($conn->connect_error) {
-       die('Connection failed: ' . $conn->connect_error);
+      die('Connection failed: ' . $conn->connect_error);
    }
-   
+
    // Validate form data
    $errors = array();
-   
+
    if (empty($_POST['fname'])) {
-       $errors[] = 'First name is required';
+      $errors[] = 'First name is required';
    }
-   
+
    if (empty($_POST['lname'])) {
-       $errors[] = 'Last name is required';
+      $errors[] = 'Last name is required';
    }
-   
+
    if (empty($_POST['email'])) {
-       $errors[] = 'Email is required';
+      $errors[] = 'Email is required';
    } else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-       $errors[] = 'Email is invalid';
+      $errors[] = 'Email is invalid';
    }
-   
+
    if (empty($_POST['phone'])) {
-       $errors[] = 'Phone is required';
+      $errors[] = 'Phone is required';
    }
-   
+
    if (empty($_POST['address'])) {
-       $errors[] = 'Address is required';
+      $errors[] = 'Address is required';
    }
-   
+
    if (empty($_POST['city'])) {
-       $errors[] = 'City is required';
+      $errors[] = 'City is required';
    }
-   
+
    if (empty($_POST['state'])) {
-       $errors[] = 'State is required';
+      $errors[] = 'State is required';
    }
-   
+
    if (empty($_POST['payment_method'])) {
       $errors[] = 'Payment method is required';
    }
-      
+
    if (empty($_POST['zip'])) {
-       $errors[] = 'Zip is required';
+      $errors[] = 'Zip is required';
    } else if (!is_numeric($_POST['zip'])) {
-       $errors[] = 'Zip is invalid';
+      $errors[] = 'Zip is invalid';
    }
-   
+
    if (count($errors) == 0) {
-       
-       // Insert data into database
-       $fname = $_POST['fname'];
-       $lname = $_POST['lname'];
-       $email = $_POST['email'];
-       $phone = $_POST['phone'];
-       $address = $_POST['address'];
-       $city = $_POST['city'];
-       $state = $_POST['state'];
-       $zip = $_POST['zip'];
-       $payment = $_POST['payment_method'];
-       $shop = $_POST['shop'];
-       $user_id = $_SESSION['userid'];
-       
-       $sql = "INSERT INTO orders (first_name, last_name, email, phone, city, state, zip, address,payment_method,shop,user_id) VALUES ('$fname', '$lname', '$email', '$phone', '$city', '$state', '$zip','$address','$payment','$shop','$user_id')";
-       
-       $cartOrder="INSERT INTO cart_order (id, product_name, product_price, size, qty, total_price, shop,user_id,status)
+
+      // Insert data into database
+      $fname = $_POST['fname'];
+      $lname = $_POST['lname'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $address = $_POST['address'];
+      $city = $_POST['city'];
+      $state = $_POST['state'];
+      $zip = $_POST['zip'];
+      $payment = $_POST['payment_method'];
+      $shop = $_POST['shop'];
+      $user_id = $_SESSION['userid'];
+
+      $sql = "INSERT INTO orders (first_name, last_name, email, phone, city, state, zip, address,payment_method,shop,user_id) VALUES ('$fname', '$lname', '$email', '$phone', '$city', '$state', '$zip','$address','$payment','$shop','$user_id')";
+
+      $cartOrder = "INSERT INTO cart_order (id, product_name, product_price, size, qty, total_price, shop,user_id,status)
 SELECT id, product_name, product_price, size, qty, total_price, shop,user_id,status
 FROM cart;";
 
       $removeCart = "TRUNCATE TABLE cart";
-     
+
       $conn->query($cartOrder);
       $conn->query($removeCart);
-     
-       
-       if ($conn->query($sql) === TRUE) {
+
+
+      if ($conn->query($sql) === TRUE) {
          echo "<script>
          swal({
              title: 'Shopping completed',
@@ -119,20 +119,20 @@ FROM cart;";
              button: 'OK'
          });
      </script>";
-       } else {
-           echo 'Error inserting data: ' . $conn->error;
-       }
-       
+      } else {
+         echo 'Error inserting data: ' . $conn->error;
+      }
+
    } else {
-       
+
    }
-   
+
    $conn->close();
-   
-   }
-   
-   
-   ?>
+
+}
+
+
+?>
 <!-- Hero section start-->
 <section class="hero" style="background-image: url('img/hero.png');">
    <div class="container">
@@ -147,15 +147,15 @@ FROM cart;";
 <!-- Register start -->
 <div class="container mt-5 mb-5">
    <?php
-      // Form is invalid. Show the errors to the user.
-      if (!empty($errors)) {
-        echo '<div class="alert alert-danger" role="alert">';
-        foreach ($errors as $error) {
-          echo '<p>' . $error . '</p>';
-        }
-        echo '</div>';
+   // Form is invalid. Show the errors to the user.
+   if (!empty($errors)) {
+      echo '<div class="alert alert-danger" role="alert">';
+      foreach ($errors as $error) {
+         echo '<p>' . $error . '</p>';
       }
-      ?>
+      echo '</div>';
+   }
+   ?>
    <div class="row">
       <div class="col-sm-8">
          <form method="POST" action="checkout.php" class="row g-3">
@@ -193,9 +193,9 @@ FROM cart;";
             <div class="col-md-4 payment-method">
                <label for="exampleFormControlTextarea1">Payment method</label>
                <div class="form-check">
-                  <input class="form-check-input" type="radio" name="payment_method" id="credit_card" value="credit_card" checked>
+                  <input class="form-check-input" type="radio" name="payment_method" id="credit_card" value="Cash on Delivery" checked>
                   <label class="form-check-label" for="credit_card">
-                  Credit Card
+                  Cash on Delivery
                   </label>
                </div>
                <div class="form-check">
@@ -210,22 +210,22 @@ FROM cart;";
       <!-- Product details section -->
       <h4>Product details</h4>
       <?php
-         $total=0;
-           while ($cart = $stmt2->fetch()) {
-            $total=$total+($cart['product_price'] * $cart['qty']);
+      $total = 0;
+      while ($cart = $stmt2->fetch()) {
+         $total = $total + ($cart['product_price'] * $cart['qty']);
          ?>
-      <div class="row">
-      <input type="hidden" name="shop" value="<?php echo $cart['shop']?>">
-      <p class="col-6"><?php echo $cart['product_name']?>-<?php echo $cart['qty']?></p>
-      <div class="col-6 text-right"><?php echo number_format(($cart['product_price'] * $cart['qty']), 2)?></div>
-      </div>
-      <?php
-         }
-         ?>
+         <div class="row">
+         <input type="hidden" name="shop" value="<?php echo $cart['shop'] ?>">
+         <p class="col-6"><?php echo $cart['product_name'] ?>-<?php echo $cart['qty'] ?></p>
+         <div class="col-6 text-right"><?php echo number_format(($cart['product_price'] * $cart['qty']), 2) ?></div>
+         </div>
+         <?php
+      }
+      ?>
       <hr class="border-bottom">
       <div class="row">
       <div class="col-6"><strong>Total amount</strong></div>
-      <div class="col-6 text-right"><?php echo number_format($total,2) ?></div>
+      <div class="col-6 text-right"><?php echo number_format($total, 2) ?></div>
       </div>
       <div class="text-center mt-4">
       <a href="checkout.php">
@@ -238,7 +238,7 @@ FROM cart;";
 </div>
 <!-- Register end -->
 <?php
-   // Include the footer file
-   require_once 'footer.php';
-   
-   ?>
+// Include the footer file
+require_once 'footer.php';
+
+?>
