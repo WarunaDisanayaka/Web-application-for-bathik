e4049ce7607b7e609df34ac835221f3a8fcff4db
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-05-01 23:27:15 +0530

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,69 +1,72 @@
 <?php
-   session_start();
+session_start();
 
-   if (!isset($_SESSION['email'])) {
-      // Redirect to the login page
-     
-      // echo("Please login");
-      exit();
-  
-  }
+if (!isset($_SESSION['email'])) {
+   // Redirect to the login page
 
-   // Connect to the database
-   $host = 'localhost'; 
-   $user = 'root'; 
-   $pwd = ''; 
-   $dbname = 'bathik'; 
-   $conn = new mysqli($host, $user, $pwd, $dbname);
-   
-   //  Checking product id 
-   if (isset($_POST['pid'])) {
-      $pid = $_POST['pid'];
-      $pname = $_POST['pname'];
-      $pprice = $_POST['pprice'];
-      $size = $_POST['size'];
-      $shop = $_SESSION['shop'];
-      $user_id = $_SESSION['userid'];
-      $qty = 1;
-   
-      $sql = "SELECT id FROM cart WHERE id='$pid'";
-      $result = $conn->query($sql);
-   
-      if ($result->num_rows == 0) {
-          $insert = "INSERT INTO cart(id,product_name,product_price,size,qty,total_price,shop,user_id,status) VALUES('$pid','$pname','$pprice','$size','$qty','$qty','$shop','$user_id','Pending')";
-          $r = $conn->query($insert);
-      }else{
-          echo "<script>
+   // echo("Please login");
+   echo "<script>
+          alert('Please login to buy items!')
+          </script>";
+   exit();
+
+}
+
+// Connect to the database
+$host = 'localhost';
+$user = 'root';
+$pwd = '';
+$dbname = 'bathik';
+$conn = new mysqli($host, $user, $pwd, $dbname);
+
+//  Checking product id 
+if (isset($_POST['pid'])) {
+   $pid = $_POST['pid'];
+   $pname = $_POST['pname'];
+   $pprice = $_POST['pprice'];
+   $size = $_POST['size'];
+   $shop = $_SESSION['shop'];
+   $user_id = $_SESSION['userid'];
+   $qty = 1;
+
+   $sql = "SELECT id FROM cart WHERE id='$pid'";
+   $result = $conn->query($sql);
+
+   if ($result->num_rows == 0) {
+      $insert = "INSERT INTO cart(id,product_name,product_price,size,qty,total_price,shop,user_id,status) VALUES('$pid','$pname','$pprice','$size','$qty','$qty','$shop','$user_id','Pending')";
+      $r = $conn->query($insert);
+   } else {
+      echo "<script>
           alert('Item already added!')
           </script>";
-      }
-   
-   
    }
-   
-   
-   //  Product count
-   if (isset($_GET['cartItem']) && isset($_GET['cartItem']) == 'cart_item') {
-   
-      $sql = "SELECT * FROM cart";
-      $result = $conn->query($sql);
-      $rows = $result->num_rows;
-      
-      echo $rows;
-   
-   
+
+
+}
+
+
+//  Product count
+if (isset($_GET['cartItem']) && isset($_GET['cartItem']) == 'cart_item') {
+
+   $sql = "SELECT * FROM cart";
+   $result = $conn->query($sql);
+   $rows = $result->num_rows;
+
+   echo $rows;
+
+
+}
+
+
+//Remove cart item
+if (isset($_GET['remove'])) {
+   $id = $_GET['remove'];
+   $remove = "DELETE FROM cart WHERE id='$id'";
+   $remove_execute = $conn->query($remove);
+
+   if ($remove_execute) {
+      header('location:cart.php');
    }
-   
-   
-   //Remove cart item
-   if (isset($_GET['remove'])) {
-      $id = $_GET['remove'];
-      $remove = "DELETE FROM cart WHERE id='$id'";
-      $remove_execute=$conn->query($remove);
-   
-      if ($remove_execute) {
-          header('location:cart.php');
-      }
-   }
-   
-   ?>
+}
+
+?>
