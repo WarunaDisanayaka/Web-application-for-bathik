1617b1e5d89f92a780161141f142e4aebd1a184c
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-05-04 22:34:10 +0530

    Update allshop.php

--- allshop.php
+++ allshop.php
@@ -7,12 +7,12 @@ $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO($dsn, $username, $password, $options);
 
 // Select all shops with images
-$stmt = $pdo->query('SELECT store_id,storename FROM stores');
+$stmt = $pdo->query('SELECT store_id,storename,banner_img FROM stores');
 
 if (isset($_GET['location'])) {
-    $location = $_GET['location'];
-    $stmt = $pdo->prepare('SELECT store_id, storename FROM stores WHERE location = ?');
-    $stmt->execute([$location]);
+  $location = $_GET['location'];
+  $stmt = $pdo->prepare('SELECT store_id, storename,banner_img FROM stores WHERE location = ?');
+  $stmt->execute([$location]);
 }
 
 ?>
@@ -40,6 +40,36 @@ require_once 'header.php';
 
 </script>
 
+<!-- Slider started -->
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/1.png" class="d-block w-100 h-100" alt="Slide 1">
+    </div>
+    <div class="carousel-item">
+      <img src="img/2.png" class="d-block w-100 h-100" alt="Slide 2">
+    </div>
+    <div class="carousel-item">
+      <img src="img/3.png" class="d-block w-100 h-100" alt="Slide 3">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </a>
+</div>
+
+<!-- Slider end -->
 
 
 <!-- Search bar -->
@@ -64,24 +94,24 @@ require_once 'header.php';
     <?php
     while ($row = $stmt->fetch()) {
 
-        ?>
-            <div class="col-md-3">
-              <div class="card border-0">
-                <img src="img/shop1.jpeg" class="card-img-top" alt="...">
-                <div class="card-body d-flex flex-column justify-content-center">
-                  <h5 class="card-title text-center"><a href="shop.php?id=<?php echo $row['store_id'] ?>"><?php echo $row['storename'] ?></a></h5>
-                  <!-- <a href="shop.php">shop</a> -->
-                  <div class="rating text-center">
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
+      ?>
+                <div class="col-md-3">
+                  <div class="card border-0">
+                    <img src="vendordashboard/<?php echo $row['banner_img'] ?>" class="card-img-top" alt="...">
+                    <div class="card-body d-flex flex-column justify-content-center">
+                      <h5 class="card-title text-center"><a href="shop.php?id=<?php echo $row['store_id'] ?>"><?php echo $row['storename'] ?></a></h5>
+                      <!-- <a href="shop.php">shop</a> -->
+                      <div class="rating text-center">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <?php
+                <?php
     }
     ?>
 
